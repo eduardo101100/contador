f90fb7fbb90bf7df974243997616c7017efa7986
eduardo101100 <eduardolopezurbano@gmail.com>
2022-03-31 10:43:39 -0600

    suma 1 numero al la cuenta

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-
+import "./app.css"
+import { useState } from "react";
 
 const App = () => {
+
+  const [valor, setValor] = useState(0)
+  const handleClick = (v) => {
+    console.log(v);
+    setValor(v+1);
+  }
   return (
     <div className="App">
     <h1>Hola mundo</h1>
-   
+    <hr />
+    <h2 className ="text-center" >{ valor }</h2>
+    <hr/>
+    <div className="margin-button">
+      <button className="btn btn-primary me-2" onClick={() => handleClick(valor)}
+      >+1</button>
+    
+    </div>
+
     </div>
   );
 }
